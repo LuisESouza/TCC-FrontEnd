f696070264cab1b032882d0c301ae93705a05419
LuisESouza <leduardosouza2204@gmail.com>
2024-10-16 18:11:14 -0400

    Create TrainingView, components e update .env

---
+++ src/components/utils/SelectExercicio.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import apiService from '../../stores/services/apiService';
+
+const exercises = ref([]);
+
+const loadExercises = async () => {
+    const url = import.meta.env.VITE_URL_EXERCICES_TEST;
+    const api = new apiService();
+
+    try {
+        const response = await api.apiGet(url);
+        exercises.value = response;
+    } catch (error) {
+        console.error('Erro ao carregar exercícios:', error);
+    }
+};
+
+onMounted(() => {
+    loadExercises();
+});
+</script>
+
+<template>
+    <div class="flex-fill">
+        <div class="select">
+            <select name="Exercicios" class="optionsInput">
+                <option v-for="(exercise, index) in exercises" :key="index">{{ exercise.nome_exercicio }}</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #1a1a1d;
+}
+.select {
+    width: 100%;
+    max-width: 400px;
+}
+.optionsInput {
+    width: 100%;
+    height: 55px;
+    padding: 0 15px;
+    border: 1px solid #323238;
+    border-radius: 4px;
+    background-color: #121214;
+    font-size: 16px;
+    color: #fff;
+    transition: border-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+    appearance: none;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'%3E%3Cpath fill='white' d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'/%3E%3C/svg%3E");
+    background-position: right 15px center;
+    background-repeat: no-repeat;
+    background-size: 12px;
+}
+.optionsInput option:checked {
+    background-color: #00875F;
+    color: #fff;
+}
+.optionsInput:focus {
+    border-color: #00875F;
+    outline: none;
+    box-shadow: 0 0 5px rgba(0, 135, 95, 0.5);
+}
+</style>
--- src/views/TrainingView.vue
+++ src/views/TrainingView.vue
@@ -1,17 +1,50 @@
 <script setup>
-    import NavInfo from '@/components/NavInfo.vue';
-    import Footer from '@/components/Footer.vue';
+import NavInfo from '@/components/NavInfo.vue';
+import Footer from '@/components/Footer.vue';
+import Inputs from '../components/utils/Inputs.vue';
+import Buttons from '../components/utils/Buttons.vue';
+import SelectExercice from '../components/utils/SelectExercicio.vue';
+import { ref } from 'vue';
+
+const isEditing = ref(false);
+
+const toggleShow = () => {
+  isEditing.value = !isEditing.value;
+};
+
+const saveForm = () => {
+  isEditing.value = false;
+};
 </script>
 
 <template>
-    <div class="container">
-        <NavInfo />
-        <main class="content">
-            
-        </main>
-        <Footer />
-    </div>
+  <div class="container">
+    <NavInfo />
+    <main class="container my-4">
+      <form v-if="isEditing" class="mb-4" @submit.prevent="saveForm">
+        <div class="row">
+          <div class="d-flex">
+            <span>Exercicios</span>
+            <SelectExercice />
+          </div>
+          
+          <Inputs />
+          <Inputs />
+        </div>
+        <Buttons titleButton="Salvar" type="submit" />
+      </form>
+
+      <div v-if="!isEditing" class="d-flex flex-column align-items-center justify-content-center" style="min-height: 60vh;">
+        <img src="@/assets/pesoImg.png" alt="Peso" class="img-fluid mb-3" style="width: 125px;">
+        <Buttons titleButton="Criar treino" id="btnMenu" @click="toggleShow" />
+      </div>
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
+#btnMenu {
+  font-size: 18px;
+}
 </style>
